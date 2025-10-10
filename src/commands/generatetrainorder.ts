5df71b67e2fce773bbe13ffb8997534e1ed7f1e5
ShiralynDev <jultomtenriktig@gmail.com>
2025-10-10 23:21:52 +0200

    Ping people in train order

--- src/commands/generatetrainorder.ts
+++ src/commands/generatetrainorder.ts
@@ -14,8 +14,33 @@ export const data = new SlashCommandBuilder()
   .addStringOption(option =>
     option
       .setName('instructionstext')
-      .setDescription('instructions text ')
+      .setDescription('instructions text')
       .setRequired(true))
+  .addMentionableOption(option =>
+    option
+      .setName('ping1')
+      .setDescription('Ping YMs or whoever u need to ping')
+      .setRequired(false))
+  .addMentionableOption(option =>
+    option
+      .setName('ping2')
+      .setDescription('Ping YMs or whoever u need to ping')
+      .setRequired(false))
+  .addMentionableOption(option =>
+    option
+      .setName('ping3')
+      .setDescription('Ping YMs or whoever u need to ping')
+      .setRequired(false))
+  .addMentionableOption(option =>
+    option
+      .setName('ping4')
+      .setDescription('Ping YMs or whoever u need to ping')
+      .setRequired(false))
+  .addMentionableOption(option =>
+    option
+      .setName('ping5')
+      .setDescription('Ping YMs or whoever u need to ping')
+      .setRequired(false))
 
 export async function execute(interaction: ChatInputCommandInteraction) {
   const member = interaction.member as GuildMember;
@@ -27,6 +52,13 @@ export async function execute(interaction: ChatInputCommandInteraction) {
   const instructionsText = interaction.options.getString("instructionstext");
   const date = `${globals.gameDateTime.toLocaleString("en-US", { month: "short" })}. ${globals.gameDateTime.getDate()}, ${globals.gameDateTime.getFullYear()}`;
   const time = globals.gameDateTime.toLocaleTimeString("en-GB", { hour: "2-digit", minute: "2-digit" });
+  const pings = [
+    interaction.options.getMentionable("ping1"),
+    interaction.options.getMentionable("ping2"),
+    interaction.options.getMentionable("ping3"),
+    interaction.options.getMentionable("ping4"),
+    interaction.options.getMentionable("ping5"),
+  ].filter((p): p is NonNullable<typeof p> => p !== null);
 
   if (!toText || !instructionsText) {
     return interaction.reply({ content: "Something failed :(", ephemeral: true });
@@ -47,7 +79,12 @@ export async function execute(interaction: ChatInputCommandInteraction) {
 
   const embed = new EmbedBuilder(embedJson);
 
-  await interaction.reply({ embeds: [embed] });
+  const pingText = pings.map(p => p.toString()).join(" ");
+    await interaction.reply({
+    content: pingText || "No pings specified.",
+    embeds: [embed],
+  });
+
   const orderMsg = await interaction.fetchReply();
   trainOrders[globals.currentTrainOrder] = orderMsg.id;
 
